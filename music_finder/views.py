1f8de82695c1beb7b928ae7984c87214b09d19b1
nonk123 <Sergeyii321@gmail.com>
2020-04-20 17:46:39 +0300

    Hopefully fix a terrible memory leak

--- music_finder/views.py
+++ music_finder/views.py
@@ -3,27 +3,26 @@ from django.http import JsonResponse
 from youtube_dl import YoutubeDL
 from youtube_search import YoutubeSearch
 
-from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
+
+dl = YoutubeDL(params={
+    "extractaudio": True,
+    "noplaylist": True,
+    "audioformat": "mp3"
+})
 
 def get_info(link):
-    params = {
-            "extractaudio": True,
-            "noplaylist": True,
-            "audioformat": "mp3"
+    info = dl.extract_info(link, download=False)
+
+    return {
+        "title": info["title"],
+        "url": info["requested_formats"][-1]["url"]
     }
-
-    with YoutubeDL(params=params) as dl:
-        info = dl.extract_info(link, download=False)
-
-        return {
-            "title": info["title"],
-            "url": info["requested_formats"][-1]["url"]
-        }
 
 def search(request, query, max_results):
     results = YoutubeSearch(query, max_results).to_dict()
     links = ["https://youtube.com/" + r["link"] for r in results]
 
     return JsonResponse({
-        "results": Pool().map(get_info, links)
+        "results": ThreadPool().map(get_info, links)
     })
