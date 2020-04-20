d3f18f1ce202624581c5d810bfa3a694b4797bb9
nonk123 <Sergeyii321@gmail.com>
2020-04-20 15:59:05 +0300

    Implement multiprocessing

--- music_finder/views.py
+++ music_finder/views.py
@@ -3,26 +3,27 @@ from django.http import JsonResponse
 from youtube_dl import YoutubeDL
 from youtube_search import YoutubeSearch
 
-dl = YoutubeDL(params={
-    "extractaudio": True,
-    "noplaylist": True,
-    "audioformat": "mp3"
-})
+from multiprocessing import Pool
+
+def get_info(link):
+    params = {
+            "extractaudio": True,
+            "noplaylist": True,
+            "audioformat": "mp3"
+    }
+
+    with YoutubeDL(params=params) as dl:
+        info = dl.extract_info(link, download=False)
+
+        return {
+            "title": info["title"],
+            "url": info["requested_formats"][-1]["url"]
+        }
 
 def search(request, query, max_results):
     results = YoutubeSearch(query, max_results).to_dict()
     links = ["https://youtube.com/" + r["link"] for r in results]
 
-    results = []
-
-    for link in links:
-        info = dl.extract_info(link, download=False)
-
-        results.append({
-            "title": info["title"],
-            "url": info["requested_formats"][-1]["url"]
-        })
-
     return JsonResponse({
-        "results": results
+        "results": Pool().map(get_info, links)
     })
